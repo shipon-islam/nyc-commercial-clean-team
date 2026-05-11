e8c3f6602b4c0fb101572d99d80b97396cf58b0e
shipon islam <shiponkhan566@gmail.com>
2026-05-11 17:09:32 +0600

    fixed topbar text

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,22 @@
 "use client";
 import { trackPhoneClick } from "@/lib/gtm";
+import { Icon } from "@iconify/react";
 export default function Topbar() {
   return (
     <div className="bg-black text-white text-center h-8 sticky top-0 z-50 flex items-center justify-center">
-        <p className="text-sm">Call Now <a onClick={()=>trackPhoneClick("topbar_phone")} className="hover:underline" href="tel:+1 (631) 381-7252">(631) 381-7252</a></p> 
+      <div className="text-sm flex items-center gap-1.5">
+        <Icon icon="tdesign:call" width="18" height="18" />
+        <p>
+          Call{" "}
+          <a
+            onClick={() => trackPhoneClick("topbar_phone")}
+            className="hover:underline"
+            href="tel:+1 (631) 381-7252"
+          >
+            (631) 381-7252
+          </a>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
