8a866bae3d474be13b33ba8c320b62c28d94fc75
shipon islam <shiponkhan566@gmail.com>
2026-05-11 16:48:34 +0600

    fixed topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,7 +3,7 @@ import { trackPhoneClick } from "@/lib/gtm";
 export default function Topbar() {
   return (
     <div className="bg-black text-white text-center h-8 sticky top-0 z-50 flex items-center justify-center">
-        <p className="text-sm">Call Now ! <a onClick={()=>trackPhoneClick("topbar_phone")} className="underline" href="tel:+1 (631) 381-7252">+1 (631) 381-7252</a></p> 
+        <p className="text-sm">Call Now <a onClick={()=>trackPhoneClick("topbar_phone")} className="hover:underline" href="tel:+1 (631) 381-7252">(631) 381-7252</a></p> 
     </div>
   )
 }
